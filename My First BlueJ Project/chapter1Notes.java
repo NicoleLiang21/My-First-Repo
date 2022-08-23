786b36d2b5a2bd749e6f392cd22ac0c5ca2ea302
Nicole Liang <111467644+NicoleLiang21@users.noreply.github.com>
2022-08-23 12:39:24 -0500

    BlueJ attempt #1

---
+++ My First BlueJ Project/chapter1Notes.java
@@ -0,0 +1,36 @@
+/**
+ * Nicole Liang
+ * 8.23.2022
+ * Notes for Chapter 1
+ * Where we're going to take some code notes!
+ * Who helped me: Mr. Hayes
+ */
+import java.util.Scanner;
+import java.util.Random;
+
+public class chapter1Notes
+{
+
+    /**
+     * main method
+     * where we are going to code 
+     */
+    public static void main(String[] args)
+    {
+        Scanner keyboard = new Scanner(System.in);
+        
+        //These are the basic data types
+        int x = 9, y, z;
+        double num = 5, num1 = 23.4567, num2; //8 bytes
+        char letter = 'd';
+        boolean choice = false;
+        float anum = 7; //1 byte
+        long a = 9; //8 byes, can put more numbers in
+        short b = 5; 
+        
+        //These are objects
+        String word = new String("Maga");
+        Integer number = new Integer(47);
+        Double number2 = new Double(34.5);
+    }
+}
